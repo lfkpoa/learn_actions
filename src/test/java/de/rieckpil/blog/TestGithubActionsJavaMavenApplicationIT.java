00317c9da483cd2ab11b09bebeb12fa9a13e2f73
Luis Fernando Kauer <luis.kauer@receita.gov.br>
2021-09-14 21:22:11 -0300

    ajustes

---
+++ src/test/java/de/rieckpil/blog/TestGithubActionsJavaMavenApplicationIT.java
@@ -0,0 +1,36 @@
+package de.rieckpil.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TestGithubActionsJavaMavenApplicationIT {
+
+  @Autowired
+  private WebTestClient webTestClient;
+
+  @Test
+  void shouldReturnThreeDefaultUser() {
+    this.webTestClient
+      .get()
+      .uri("/api/users")
+      .exchange()
+      .expectStatus()
+      .isOk()
+      .expectBody()
+      .jsonPath("$.length()").isEqualTo(3);
+  }
+
+  @Test
+  void testError() {
+	  this.webTestClient
+	  .get()
+      .uri("/api/users")
+      .exchange()
+      .expectStatus()
+      .isNotFound();
+  }
+  
+}
